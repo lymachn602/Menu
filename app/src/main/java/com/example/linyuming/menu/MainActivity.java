216e0fcb319b05b2e504ad4f71a56a88619e5dc9
lymachn602 <287643905@qq.com>
2017-01-01 21:40:11 +0800

    Initial commit

---
+++ app/src/main/java/com/example/linyuming/menu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.linyuming.menu;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView tv1;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+}
